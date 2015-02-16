e9c253510144b65ef30b90335062b49c86d55dc8
Joni Salmi <josalmi@cs.helsinki.fi>
2015-02-16 11:26:05 +0200

    Travis disable animations and use cache for AVD

---
+++ avd-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+function disable_animation() {
+  echo "Disabling animations"
+  echo "    Opening Developer Settings"
+  adb shell am start -W -n com.android.settings/.DevelopmentSettings
+  echo "    Scroll to Window animation scale"
+  for i in {1..23}; do
+    adb shell input keyevent 20
+  done
+  echo "    Set Window animation, Transition scale and Animator duration scale to 0x"
+  for i in {1..3}; do
+    adb shell input keyevent 20
+    adb shell input keyevent 23
+    adb shell input keyevent 19
+    adb shell input keyevent 19
+    adb shell input keyevent 23
+  done
+  adb shell input keyevent 3
+  echo "Done"
+}
+
+if [ ! -d "$HOME/.android/avd/$NAME.avd" ] || [ ! -f "$HOME/.android/avd/$NAME.ini" ]; then 
+  echo "no" | android create avd --force -n "$NAME" -t "$TARGET" --abi armeabi-v7a
+  emulator -avd "$NAME" -no-audio -no-window &
+  android-wait-for-emulator
+  adb shell input keyevent 82
+  disable_animation 
+  adb shell sqlite3 "/data/data/com.android.providers.settings/databases/settings.db" ".dump"
+else
+  echo "Using AVD cache"
+  emulator -avd "$NAME" -no-audio -no-window &
+  android-wait-for-emulator
+  adb shell input keyevent 82 &
+fi
